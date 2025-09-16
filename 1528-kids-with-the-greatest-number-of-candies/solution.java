15241b5ee3e1bfc44bd9eb160ee0a83f987fa0f0
Jack Carfrey <109220285+jarfrey@users.noreply.github.com>
2025-09-16 22:20:39 +0000

    [LeetCode Sync] Runtime - 1 ms (96.44%), Memory - 42.3 MB (99.99%)

---
+++ 1528-kids-with-the-greatest-number-of-candies/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList();
+       // boolean[] result = new boolean[candies.length];
+        int maxCandies = 0;
+        for (int i = 0; i<candies.length;i++){
+            if (candies[i] > maxCandies){
+                maxCandies = candies[i];
+            }
+        }
+        for (int i = 0; i<candies.length;i++){
+            if ((candies[i] + extraCandies) >= maxCandies){
+                result.add(true);
+            }else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
