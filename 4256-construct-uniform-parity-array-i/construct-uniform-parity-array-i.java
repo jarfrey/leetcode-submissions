02b84c0ea453ba84573fd3dac52e0afffa27328e
Jack Carfrey <109220285+jarfrey@users.noreply.github.com>
2026-09-02 15:25:48 -0400

    Time: 0 ms (100.00%) | Memory: 44.8 MB (97.05%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+
+// [1, 1]
+// [1, 1]
+
+// [2, 3]
+// [-1, 3]
+
+// [3, 2]
+// [3, -1]
+
+// [2, 1, 4]
+// [1, 1, 3]
+
+// [3, 2, 5]
+// [3, -1, 5]
